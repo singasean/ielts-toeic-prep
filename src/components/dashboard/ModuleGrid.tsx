ca367780e33e2204ce25482ceafeee0bd0b3c0f6
singasean <seanteo22@gmail.com>
2025-12-17 10:44:08 +0700

    Add hskLevel filtering to ModuleGrid - hide Writing for HSK levels 1-2

--- src/components/dashboard/ModuleGrid.tsx
+++ src/components/dashboard/ModuleGrid.tsx
@@ -1,12 +1,19 @@
 import { useExam } from '@/contexts/ExamContext';
 import { ModuleCard } from './ModuleCard';
 
-export const ModuleGrid = () => {
+export const ModuleGrid = ({ hskLevel }  => {
   const { modules } = useExam();
+    const filteredModules = modules.filter((module) => {
+    // Hide Writing module for HSK levels 1-2
+    if (hskLevel && hskLevel < 3 && module.id === 'hsk-writing') {
+      return false;
+    }
+    return true;
+  });
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      {modules.map((module, index) => (
+      {filteredModules.map((module, index) => (
         <ModuleCard key={module.id} module={module} index={index} />
       ))}
     </div>
